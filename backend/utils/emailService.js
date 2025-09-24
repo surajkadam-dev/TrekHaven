e72ca96c1aded8631c77502911d0e015f60036ba
IsSurajKadam <surajkadam1706004@gmail.com>
2025-09-24 23:00:31 +0530

    Update emailService.js

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -5,15 +5,21 @@ import nodemailer from "nodemailer";
 
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  service: process.env.SMTP_SERVICE,
+  host: "smtp.gmail.com",       // don’t use "service" here
+  port: 465,                    // SSL
+  secure: true,                 // true because port 465
   auth: {
-    user: process.env.SMTP_MAIL,
-    pass: process.env.SMTP_PASSWORD
+    user: process.env.SMTP_MAIL,      // your Gmail
+    pass: process.env.SMTP_PASSWORD,  // your Gmail App Password
+  },
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP error:", error);
+  } else {
+    console.log("Server is ready to send emails");
   }
 });
-
 export const sendEmail = async ({ to, subject, text,html }) => {
   await transporter.sendMail({
     from:`"Karpewadi Homestay" <${process.env.SMTP_MAIL}>`,
